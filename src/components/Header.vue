5903406d28b56d0b2064dc424850ab5af77de339
Reihane Yaghoobzade <reihanygh2002@gmail.com>
2022-03-13 16:56:57 +0330

    search input and spinner

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,38 @@
 <template>
-    <header class="container mx-auto mt-6 flex flex-col gap-3">
-        <img class="mx-auto" :src="item.values['c-l7DcP3ny0V']" width="150">
-        <h1 class="text-center text-2xl font-semibold cursor-default text-gray-700 mb-4" >لیست محصولات فروشگاه {{ item.name }}</h1>
-        <input type="text" class="outline-none px-3 py-2 my-4 border rounded-lg focus:ring-red-200 focus:ring-2 focus:border-red-500"
-        placeholder="نام محصول مورد نظرتان را وارد کنید ...">
+    <header class="container mx-auto">
+        <div class="mt-6 flex flex-col gap-3">
+            <img class="mx-auto" :src="item.values['c-l7DcP3ny0V']" width="150">
+            <h1 class="text-center text-2xl font-semibold cursor-default text-gray-700 mb-4" >لیست محصولات فروشگاه {{ item.name }}</h1>
+            <input @keyup="check" v-model.trim="search" type="text" class="outline-none px-3 py-2 mx-2 sm:mx-0 border rounded-lg focus:ring-red-200 focus:ring-2 focus:border-red-500"
+            placeholder="نام محصول مورد نظرتان را وارد کنید ...">
+        </div>
     </header>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
     props:{
         item: Object,
+    },
+    setup() {
+    const search = ref();
+
+    function check() {
+        const card = document.querySelectorAll('.card');
+        for (let i = 0; i < card.length; i++) {
+            const cardTitle = card[i].querySelector('.card-title');
+            const txtValue = cardTitle.textContent || cardTitle.innerText;
+            if (txtValue.indexOf(search.value) > -1) {
+                card[i].classList.remove('hidden');
+            } else {
+                card[i].classList.add('hidden');
+            }
+        }
     }
+
+    return { search, check };
+  },
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <template>
     <Header :item="brandItems[0]"/>
-    <main class="flex-1">
-        <section v-if="!loading" class="container mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 my-6">
+    <div v-if="loading" class="flex-1 my-20">
+      <div class="w-10 h-10 border-4 border-red-600 border-t-transparent border-solid rounded-full animate-spin mx-auto"></div>
+    </div>
+    <main v-else class="flex-1 mx-2 sm:mx-0">
+      <div class="flex-1">
+        <section class="container mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 my-6">
                 <ProductCard v-for="item in cardItems" :key="item.index" :cardItem="item.values" :phoneNumber="brandItems[0]"></ProductCard>
         </section>
+      </div>
     </main>
     <Footer :item="brandItems[0]"/>
 </template>

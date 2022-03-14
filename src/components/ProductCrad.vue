6e0d59dd06b7a022232080937c1a1fd93d68ccd0
Heydar omid <heydaromid2@gmail.com>
2022-03-14 21:04:08 +0330

    Change mobile product card view

--- src/components/ProductCrad.vue
+++ src/components/ProductCrad.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="border rounded-lg p-4 flex flex-col justify-between gap-4 card">
-      <div class="flex flex-col justify-around gap-3">
-        <img :src="cardItem['c-WoMta3EdKU']" alt="" class="block mx-auto h-36 sm:h-52">
-        <div class="flex flex-col justify-around gap-3">
+      <div class="flex sm:flex-col sm:justify-around gap-3">
+        <div class="w-1/3 sm:w-auto">
+          <img :src="cardItem['c-WoMta3EdKU']" alt="" class="block mx-auto h-36 sm:h-52">
+        </div>
+        <div class="flex flex-col justify-between w-2/3 sm:w-auto gap-3">
           <h3 class="font-semibold text-lg text-gray-700 card-title">{{ cardItem['c-zNhlqGccUo'] }}</h3>
-          <p class="font-light text-gray-600">{{ cardItem['c-ehIiJHuNqx'] }}</p>
+          <p class="font-light text-gray-600 line-clamp-2">{{ cardItem['c-ehIiJHuNqx'] }}</p>
+          <div class="flex justify-between sm:hidden">
+            <a :href="`tel:${phoneNumber.values['c-BdNc0saeof']}`" class="bg-red-500 hover:bg-red-600 text-white rounded-full py-1 px-4">سفارش</a>
+            <div class="flex items-center gap-1">
+                <span>{{ cardItem['c-GMAP2hhQnB'] }}</span>
+                <img src="../assets/Images/Toman.svg" width="18">
+            </div>
+          </div>
         </div>
       </div>
-      <div class="flex justify-between">
+      <div class="justify-between hidden sm:flex">
           <a :href="`tel:${phoneNumber.values['c-BdNc0saeof']}`" class="bg-red-500 hover:bg-red-600 text-white rounded-full py-1 px-4">سفارش</a>
           <div class="flex items-center gap-1">
               <span>{{ cardItem['c-GMAP2hhQnB'] }}</span>

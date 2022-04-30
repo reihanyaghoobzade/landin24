6ec46f0def2f5c5620c93a01eb6d74f9db7a93fd
Reihane Yaghoobzade <reihanygh2002@gmail.com>
2022-04-30 18:17:40 +0430

    add categories

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,55 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import { ref, provide } from 'vue';
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
+import { RouterLink, RouterView } from "vue-router";
+import { ref, provide } from "vue";
+import Header from "./components/Header.vue";
+import Categories from "./components/Categories.vue";
+import Footer from "./components/Footer.vue";
 const docId = "r3wHCIbocr";
 const brandTableId = "grid-5s_DTzk4lU";
 const cardsTableId = "grid-OxkfyQYjSp";
+const categoryTableId = "grid-QvGnUIqLrZ";
 const token = "d5b1f114-1d0a-45af-9138-d6b75d520488";
 const brandUrl = `https://coda.io/apis/v1/docs/${docId}/tables/${brandTableId}/rows`;
 const cardsUrl = `https://coda.io/apis/v1/docs/${docId}/tables/${cardsTableId}/rows`;
+const categoryUrl = `https://coda.io/apis/v1/docs/${docId}/tables/${categoryTableId}/rows`;
 const cardsLoading = ref(true);
 const brandItems = ref([]);
 const cardItems = ref([]);
+const categoryItems = ref([]);
 
-
-function getData(url, res, loading = ref(null)){
+function getData(url, res, loading = ref(null)) {
   fetch(url, {
-    method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
-      },
-    })
-    .then(response => response.json())
-    .then(data => {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
       res.value = data.items;
       loading.value = false;
-      })
+    })
     .catch((error) => {
-        console.error('Error: ', error);
+      console.error("Error: ", error);
     });
 }
 
 getData(brandUrl, brandItems);
 getData(cardsUrl, cardItems, cardsLoading);
+getData(categoryUrl, categoryItems);
 
-provide('brandItems', brandItems);
+provide("brandItems", brandItems);
 provide("cardItems", cardItems);
 provide("cardsLoading", cardsLoading);
+provide("categoryItems", categoryItems);
 </script>
 
 <template>
-  <Header :item="brandItems[0]"/>
+  <Header :item="brandItems[0]" />
+  <Categories />
   <router-view></router-view>
-  <Footer :item="brandItems[0]"/>
+  <Footer :item="brandItems[0]" />
 </template>
 
 <style>
---
+++ src/components/Categories.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class=" container mx-auto my-10 flex gap-8">
+        <a href="#" v-for="item in categoryItems" :key="item.index" @click="filterSearch(item.name)" class="inline-flex">{{ item.name }}</a>
+    </div>
+</template>
+
+<script setup>
+import { ref, inject } from 'vue';
+
+const filter = ref([]);
+const categoryItems = inject("categoryItems");
+const cardItems = inject("cardItems");
+
+function filterSearch(name){
+    const card = document.querySelectorAll('.card');
+    card.forEach(element => element.classList.add("hidden"));
+    !filter.value.includes(name) && filter.value.push(name);
+    filter.value.map(element => {
+        for (let i = 0; i < cardItems.value.length; i++) {
+            console.log(cardItems.value[i].values['c-DXR1PG-fhS']);
+            cardItems.value[i].values['c-DXR1PG-fhS'] === element &&
+                card[i].classList.remove("hidden");
+        }
+    });
+}
+
+</script>
+
+<style>
+
+</style>
